6f44db989e2c3a300b37563158ae9e90c212162c
Jonathan Tadese <47608480+jontad@users.noreply.github.com>
2021-12-13 15:52:17 +0100

    Conversion from class diagram to code

---
+++ User.java
@@ -0,0 +1,166 @@
+
+/********************* PAYMENT METHOD *********************/
+
+interface PaymentMethod {
+
+    public boolean pay();
+
+}
+
+class Swish implements PaymentMethod {
+    private String phoneNumber;
+
+    public Swish(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean pay() {
+        return true;
+    }
+
+}
+
+class PayPal implements PaymentMethod {
+    private String email;
+
+    public PayPal(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean pay() {
+        return true;
+    }
+
+}
+
+class CreditCard implements PaymentMethod {
+    private String creditCardNrNumber;
+    private String expiredDate;
+    private int cvc;
+    public String name;
+
+    public CreditCard(String creditCardNrNumber,  String expiredDate, int cvc, String name) {
+        this.creditCardNrNumber = creditCardNrNumber;
+        this.expiredDate = expiredDate;
+        this.cvc = cvc;
+        this.name = name;
+    }
+
+    @Override
+    public boolean pay() {
+        return true;
+    }
+
+}
+
+/********************* Account *********************/
+
+abstract class Account {
+
+    private String email;
+    private String password;
+    private boolean adminAccountStatus = false;
+
+
+    public Account(String email, String password, boolean adminAccountStatus) {
+        this.email = email;
+        this.password = password;
+        this.adminAccountStatus = adminAccountStatus;
+    }
+
+    public boolean getAdminAccountStatus() {
+        return false;
+    }
+
+    public boolean signIn(User user) {
+        return true;
+    }
+
+    public boolean signOut(User user) {
+        return true;
+    }
+}
+
+class UserAccount extends Account {
+
+    public UserAccount(String email, String password) {
+        super(email, password, false);
+    }
+
+}
+
+class AdminAccount extends Account {
+
+    public AdminAccount(String email, String password) {
+        super(email, password, true);
+    }
+
+    public void deleteUserAccount(UserAccount account) {
+
+    }
+
+    public void sendWarning(UserAccount account) {
+
+    }
+}
+
+/********************* MATCH *********************/
+
+class Match{
+    public boolean liked;
+    public String id; 
+
+    public Match(boolean liked, String id){
+        this.liked = liked;
+        this.id = id;
+    }
+}
+
+/********************* USER *********************/
+
+
+public class User {
+
+    public String name;
+    public int age;
+    public int userID;
+
+    public String location;
+    public String language;
+
+    private PaymentMethod[] paymentMethods;
+    public Match[] matches;
+
+    public User(String name, int age, int userID,
+                String location, String language, 
+                PaymentMethod[] paymentMethods, Match[] matches
+                )
+    {
+        this.name = name;
+        this.age = age;
+        this.userID = userID;
+        this.location = location;
+        this.language = language;
+        this.paymentMethods = paymentMethods;
+    }
+
+    public User getMatch(User user) {
+        return user;
+    }
+
+    public void createProfile(String bio, String tags, String picture) {
+        return;
+    }
+
+    public void like(User user) {
+        return;
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Everything compiles!");
+
+    }
+
+}
